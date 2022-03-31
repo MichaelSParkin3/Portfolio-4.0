6cf041e5acce86a192862b2345fc1d2cc06f5c86
DESKTOP-TBJ6PEF\scott <scotty495@yahoo.com>
2022-03-30 19:27:12 -0700

    init state for isIOS as true for default then set isIOSState to isIOS in useEffect

--- src/components/projectScreenMobileAlert.js
+++ src/components/projectScreenMobileAlert.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { motion } from "framer-motion"
 import { useInView } from "react-intersection-observer"
 import { isIOS } from "react-device-detect"
@@ -7,9 +7,15 @@ import "../scss/fullPageNumber.scss"
 
 const ProjectScreenMobileAlert = props => {
 
+  const [isIOSState, setIsIOSState] = useState(true)
+
+  useEffect(() => {
+    setIsIOSState(isIOS)
+  });
+
     return (
 
-    <div className={'mobileAlert'} style={isIOS ? { display: 'flex' } : { display: 'none' }}>
+    <div className={'mobileAlert'} style={isIOSState ? { display: 'flex' } : { display: 'none' }}>
       Tap twice to open a project!
       </div>
   
