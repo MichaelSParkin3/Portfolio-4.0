15b0f3c3585346c1ba3aeaa2d9296cccb7260c53
Alistar\Work <scotty495@yahoo.com>
2022-04-28 17:23:15 -0700

    Added correct copy text to otherWork page

--- src/pages/otherWork.js
+++ src/pages/otherWork.js
@@ -55,17 +55,10 @@ const OtherWork = ({ location }) => {
       <div className="project-page">
         <FullPageNumber url={location} number="04" />
         <ContFiftyFifty
-          right={
-            '<h1>ShaneCo Quizes & Components</h1><span className="divider"></span><p>I worked with Shaneco. and their team to re-develop their product suggestion quizes, create new re-usable components, and fix some cookie related problems they were having.</p>'
-          }
-        />
-
-        <ContFiftyFifty left={"<h1>Html</br>Css</br>Javascript</h1>"} />
-        <ContFiftyFifty
           rightImg={data.verb2.childImageSharp.fluid}
           setImgWidth={"280px"}
           left={
-            '<h1>The Quizzes</h1><span className="divider"></span><p>I re-developed their suggestion quiz into a re-usable and customizable component by building upon their existing code. The quiz asks the user questions and based on their answer combinations the user is reccomended different products to buy from the Shaneco store.</p><a href="https://www.shaneco.com/engagement-wedding/engagement-rings/style-finder?promo_name=nav&promo_id=engagement&promo_creative=stylefinder" target="_blank" class="link link--mneme">Find It Here</a> '
+            '<h1>Verb Labs</h1><span className="divider"></span><p>The NFT company Verb Labs asked me to make them a minimalistic webpage with their social links.</p><a href="https://verblabs.io/" target="_blank" class="link link--mneme">Find It Here</a> '
           }
         />
         <ContFiftyFifty
