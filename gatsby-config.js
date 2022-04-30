f4e131742bf675497564ac6d65929fb8d3fa11bf
Alistar\Work <scotty495@yahoo.com>
2022-04-30 16:59:33 -0700

    Added new icons and title data

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,13 +34,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Michael S Parkin III Developer Portfolio`,
+        short_name: `MSP3 Dev`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000`,
+        theme_color: `#fff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `public\icons\icon2_512x512.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
