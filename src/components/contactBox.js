485ef42ba36fca81cf590a48969a40775e909f2c
Michael Parkin <michael@lamproslabs.com>
2024-12-30 11:11:26 -0800

    added terminal effect

--- src/components/contactBox.js
+++ src/components/contactBox.js
@@ -1,39 +1,182 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import anime from "animejs"
 import "../scss/contactBox.scss"
 import cyberCoder from "../images/videos/CyberpunkCoder.mp4"
 
 const ContactBox = () => {
   const [videoLoaded, setVideoLoaded] = useState(false)
+  const videoRef = useRef(null);
+
+  var TerminalEmulator = {
+    init: function (screen) {
+      var inst = Object.create(this)
+      inst.screen = screen
+      inst.createInput()
+
+      return inst
+    },
+
+    createInput: function () {
+      var inputField = document.createElement("div")
+      var inputWrap = document.createElement("div")
+
+      inputField.className = "terminal_emulator__field"
+      inputField.innerHTML = ""
+      inputWrap.appendChild(inputField)
+      this.screen.appendChild(inputWrap)
+      this.field = inputField
+      this.fieldwrap = inputWrap
+    },
+
+    enterInput: function (input) {
+      return new Promise((resolve, reject) => {
+        var randomSpeed = (max, min) => {
+          return Math.random() * (max - min) + min
+        }
+
+        var speed = randomSpeed(70, 90)
+        var i = 0
+        var str = ""
+        var type = () => {
+          str = str + input[i]
+          this.field.innerHTML = str.replace(/ /g, "&nbsp;")
+          i++
+
+          setTimeout(() => {
+            if (i < input.length) {
+              if (i % 5 === 0) speed = randomSpeed(80, 120)
+              type()
+            } else {
+              console.log("tick")
+              setTimeout(() => {
+                console.log("tock")
+                resolve()
+              }, 400)
+            }
+          }, speed)
+        }
+
+        type()
+      })
+    },
+
+    enterCommand: function () {
+      return new Promise((resolve, reject) => {
+        var resp = document.createElement("div")
+        resp.className = "terminal_emulator__command"
+        resp.innerHTML = this.field.innerHTML
+        this.screen.insertBefore(resp, this.fieldwrap)
+
+        this.field.innerHTML = ""
+        resolve()
+      })
+    },
+
+    enterResponse: function (response) {
+      return new Promise((resolve, reject) => {
+        var resp = document.createElement("div")
+        resp.className = "terminal_emulator__response"
+        resp.innerHTML = response
+        this.screen.insertBefore(resp, this.fieldwrap)
+
+        resolve()
+      })
+    },
+
+    wait: function (time, busy) {
+      busy = busy === undefined ? true : busy
+      return new Promise((resolve, reject) => {
+        if (busy) {
+          this.field.classList.add("waiting")
+        } else {
+          this.field.classList.remove("waiting")
+        }
+        setTimeout(() => {
+          resolve()
+        }, time)
+      })
+    },
+
+    reset: function () {
+      return new Promise((resolve, reject) => {
+        this.field.classList.remove("waiting")
+        resolve()
+      })
+    },
+  }
 
   useEffect(() => {
-    const videoElement = document.getElementById("cyberCoderVideo")
-    videoElement.onloadeddata = () => {
-      setVideoLoaded(true)
+    const videoElement = videoRef.current;
+    if (videoElement) {
+      const handleLoadedData = () => {
+        console.log("Video loaded");
+        setVideoLoaded(true);
+      };
+  
+      const handleError = () => {
+        console.error("Failed to load video");
+      };
+  
+      videoElement.addEventListener("loadeddata", handleLoadedData);
+      videoElement.addEventListener("error", handleError);
+  
+      // Cleanup event listeners on component unmount
+      return () => {
+        videoElement.removeEventListener("loadeddata", handleLoadedData);
+        videoElement.removeEventListener("error", handleError);
+      };
+    }
+  }, []);
+
+  const animateImgCovers = () => {
+    console.log('running animate');
+  
+    return new Promise((resolve) => {
+      anime({
+        targets: ".img-cover-1",
+        width: "0%",
+        duration: 1000,
+        easing: "easeInOutCirc",
+        complete: resolve, // Resolve the promise when animation completes
+      });
+      anime({
+        targets: ".img-cover-2",
+        width: "0%",
+        duration: 1000,
+        easing: "easeInOutCirc",
+        complete: resolve, // Resolve the promise when animation completes
+      });
+    });
+  };
+
+  useEffect(() => {
+    const screenElement = document.getElementById("screen")
+    if (screenElement) {
+      const TE = TerminalEmulator.init(screenElement)
+      TE.wait(1000, false)
+        .then(TE.enterInput.bind(TE, "npm install websitecontroller"))
+        .then(TE.enterCommand.bind(TE))
+        .then(TE.enterResponse.bind(TE, "npm installing packages..."))
+        .then(TE.wait.bind(TE, 600))
+        .then(TE.enterResponse.bind(TE, "- remotecontroller v9 installed."))
+        .then(TE.wait.bind(TE, 300))
+        .then(
+          TE.enterResponse.bind(TE, "- 10,000 dependencies installed. ")
+        )
+        .then(TE.wait.bind(TE, 600, false))
+        .then(TE.enterInput.bind(TE, "node openblinds.js"))
+        .then(TE.enterCommand.bind(TE))
+        .then(TE.wait.bind(TE, 400))
+        .then(TE.enterResponse.bind(TE, "opening blinds..."))
+        .then(TE.wait.bind(TE, 600))
+        .then(()=>{animateImgCovers()})
+        .then(TE.wait.bind(TE, 1000))
+        .then(TE.enterResponse.bind(TE, "blinds opened successfully"))
+        .then(TE.wait.bind(TE, 600, false))
+        .then(TE.enterInput.bind(TE, "node showcontacts.js"))
+        .then(TE.reset.bind(TE))
     }
   }, [])
-
-  useEffect(() => {
-    if (videoLoaded) {
-      const animationTimeout = setTimeout(() => {
-        anime({
-          targets: ".img-cover-1",
-          width: "0%",
-          duration: 1000,
-          easing: "easeInOutCirc",
-        })
-        anime({
-          targets: ".img-cover-2",
-          width: "0%",
-          duration: 1000,
-          easing: "easeInOutCirc",
-        })
-      }, 250) // 500 milliseconds delay
-
-      // Cleanup function to clear the timeout if the component unmounts
-      return () => clearTimeout(animationTimeout)
-    }
-  }, [videoLoaded])
 
   return (
     <div className="contactBox">
@@ -43,7 +186,7 @@ const ContactBox = () => {
       </div>
       <div className="links-cont">
         <div className="links">
-          <a
+          {/* <a
             target="_blank"
             rel="noopener noreferrer"
             href="mailto:MichaelSParkin3@gmail.com"
@@ -63,10 +206,13 @@ const ContactBox = () => {
             href="https://www.linkedin.com/in/michaelscottparkin3/"
           >
             linkedin.com/in/michaelscottparkin3/
-          </a>
+          </a> */}
+          <div className="screen">
+            <div id="screen" className="terminal_emulator"></div>
+          </div>
         </div>
       </div>
-      <video id="cyberCoderVideo" src={cyberCoder} autoPlay muted loop />
+      <video ref={videoRef} id="cyberCoderVideo" src={cyberCoder} autoPlay muted loop />
       <div class="dark-overlay"></div>
     </div>
   )
